9529fd8e427c7ac662610235f12409c7afcfd7c9
Bill Lubanovic <bill@madscheme.com>
2023-11-03 01:38:50 -0500

    Add web creature, exolorer modules

---
+++ web/creature.py
@@ -0,0 +1,51 @@
+import os
+from pathlib import Path
+
+from fastapi import APIRouter, HTTPException, Body
+from fastapi.templating import Jinja2Templates
+
+from model.creature import Creature
+if os.getenv("CRYPTID_UNIT_TEST"):
+    print("web fake")
+    print("env=", os.getenv("CRYPTID_UNIT_TEST"))
+    from fake import creature as service
+else:
+    print("web real")
+    from service import creature as service
+from error import Missing, Duplicate
+
+router = APIRouter(prefix = "/creature")
+top = Path(__file__).resolve().parent
+templates = Jinja2Templates(directory=f"{top}/template")
+
+@router.get("/")
+def get_all() -> list[Creature]:
+    return service.get_all()
+
+@router.get("/{name}")
+def get_one(name: str) -> Creature:
+    try:
+        return service.get_one(name)
+    except Missing as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
+
+@router.post("/", status_code=201)
+def create(creature: Creature) -> Creature:
+    try:
+        return service.create(creature)
+    except Duplicate as exc:
+        raise HTTPException(status_code=409, detail=exc.msg)
+
+@router.patch("/{name}")
+def modify(name: str, creature: Creature) -> Creature:
+    try:
+        return service.modify(name, creature)
+    except Missing as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
+
+@router.delete("/{name}")
+def delete(name: str) -> None:
+    try:
+        return service.delete(name)
+    except Missing as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
---
+++ web/explorer.py
@@ -0,0 +1,44 @@
+import os
+from fastapi import APIRouter, HTTPException
+from model.explorer import Explorer
+if os.getenv("CRYPTID_UNIT_TEST"):
+    from fake import explorer as service
+else:
+    from service import explorer as service
+from error import Duplicate, Missing
+
+router = APIRouter(prefix = "/explorer")
+
+@router.get("")
+@router.get("/")
+def get_all() -> list[Explorer]:
+    return service.get_all()
+
+@router.get("/{name}")
+def get_one(name) -> Explorer:
+    try:
+        return service.get_one(name)
+    except Missing as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
+
+@router.post("", status_code=201)
+@router.post("/", status_code=201)
+def create(explorer: Explorer) -> Explorer:
+    try:
+        return service.create(explorer)
+    except Duplicate as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
+
+@router.patch("/")
+def modify(name: str, explorer: Explorer) -> Explorer:
+    try:
+        return service.modify(name, explorer)
+    except Missing as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
+
+@router.delete("/{name}", status_code=204)
+def delete(name: str):
+    try:
+        return service.delete(name)
+    except Missing as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
