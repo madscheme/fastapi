c88f0aa8e15a7fcb93cae6f95a69f444d7ab4f10
Bill Lubanovic <bill@madscheme.com>
2023-11-03 00:41:30 -0500

    Anchor sqlite db undee top /db dir

---
+++ data/init.py
@@ -0,0 +1,28 @@
+"""Initialize SQLite database"""
+
+import os
+from pathlib import Path
+from sqlite3 import connect, IntegrityError, Connection, Cursor
+from error import Missing, Duplicate
+
+conn: Connection | None = None
+curs: Cursor | None = None
+
+def get_db(name: str|None = None, reset: bool = False):
+    """Connect to SQLite database file"""
+    global conn, curs
+    if conn:
+        if not reset:
+            return
+        conn = None
+    name = os.getenv("CRYPTID_SQLITE_DB")
+    if not name:
+        top_dir = Path(__file__).resolve().parents[1] # repo top
+        db_dir = top_dir / "db"
+        db_name = "cryptid.db"
+        db_path = str(db_dir / db_name)
+        name = os.getenv("CRYPTID_SQLITE_DB", db_path)
+    conn = connect(name, check_same_thread=False)
+    curs = conn.cursor()
+
+get_db()
