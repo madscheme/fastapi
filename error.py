99c96eed429b769b5c267a1362e9980cbdd98c97
Bill Lubanovic <bill@madscheme.com>
2023-11-03 02:03:21 -0500

    Add errors

---
+++ error.py
@@ -0,0 +1,9 @@
+# Data exceptions
+
+class Missing(Exception):
+    def __init__(self, msg:str):
+        self.msg = msg
+
+class Duplicate(Exception):
+    def __init__(self, msg:str):
+        self.msg = msg
