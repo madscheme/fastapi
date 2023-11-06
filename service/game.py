9153b49922601268ee8a6bfe8aa6a5133078a830
Bill Lubanovic <bill@madscheme.com>
2023-11-05 19:34:30 -0600

    add servide game.py

--- service/game.py
+++ service/game.py
@@ -1,23 +1,31 @@
+from collections import Counter, defaultdict
 import data.game as data
 
-HIT = "H "  # right letter and position
+HIT = "H"  # right letter and position
 MISS = "M"  # letter not in word
 CLOSE = "C" # letter in word but wrong position
+ERROR = ""
 
 def get_word() -> str:
     return data.get_word()
 
-def get_score(word: str, guess: str) -> str:
-    word = word.lower()
-    guess = guess.lower()
-    score: str = ""
-    if len(word) == len(guess):
-        for pos, letter in enumerate(guess):
-            if letter in word:
-                if letter == word[pos]:
-                    score += HIT
-                else:
-                    score += CLOSE
-            else:
-                score += MISS
-    return score
+def get_score(actual: str, guess: str) -> str:
+    length: int = len(actual)
+    if len(guess) != length:
+        return ERROR
+    actual_counter = Counter(actual) #  {letter: count, ...}
+    guess_counter = defaultdict(int)
+    result = [MISS] * length
+    for pos, letter in enumerate(guess):
+        if letter == actual[pos]:
+            result[pos] = HIT
+            guess_counter[letter] += 1
+    for pos, letter in enumerate(guess):
+        if result[pos] == HIT:
+            continue
+        guess_counter[letter] += 1
+        if (letter in actual and
+            guess_counter[letter] <= actual_counter[letter]):
+            result[pos] = CLOSE
+    result = ''.join(result)
+    return result
