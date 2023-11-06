9ebe3a1084d4ebeac16409cb0fbe41b162bbb1bc
Bill Lubanovic <bill@madscheme.com>
2023-11-06 09:48:35 -0600

    add user

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from web import creature, explorer, game
+from web import creature, explorer, game, user
 
 app = FastAPI()
 
 app.include_router(explorer.router)
 app.include_router(creature.router)
 app.include_router(game.router)
+app.include_router(user.router)
 
 if __name__ == "__main__":
     import uvicorn
